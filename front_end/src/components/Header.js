14423b0a4494a566605cb51a7f888667aff84b10
unknown <adheesh.kv@sgstechsolutions.com>
2024-09-16 12:02:02 +0530

    sgs-header

--- front_end/src/components/Header.js
+++ front_end/src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div>
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img className="mx-auto h-10 w-auto animate-pulse" src="https://files.sitebuilder.name.tools/a5/a2/a5a290aa-65a5-46eb-9524-b5afe446d4d5.png" alt="sgs "></img>
+            <img className="mx-auto h-10 w-auto" src="https://files.sitebuilder.name.tools/a5/a2/a5a290aa-65a5-46eb-9524-b5afe446d4d5.png" alt="sgs "></img>
         </div>
     </div>
   )
